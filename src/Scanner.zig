057bef9a502b7479f9b6c397ebd3d4c2a35711bb
Jellli <dt@live.it>
2026-02-07 16:51:04 +0800

    4.4

--- src/Scanner.zig
+++ src/Scanner.zig
@@ -1,24 +1,61 @@
 const std = @import("std");
-const Token = @import("Token.zig").Token;
+const Token = @import("Token.zig");
+const Allocator = std.mem.Allocator;
 
-var start: u32 = 0;
-var current: u32 = 0;
-var line: u32 = 1;
+const Self = @This();
 
-const Scanner = struct {
-    source: []const u8,
-    tokens: std.ArrayList(Token),
+start: u32 = 0,
+current: u32 = 0,
+line: u32 = 1,
+allocator: Allocator,
+source: []const u8,
+tokens: std.ArrayList(Token),
 
-    fn init(source: []const u8) @This() {
-        return Scanner{
-            .source = source,
-            .tokens = .empty,
-        };
+fn init(allocator: Allocator, source: []const u8) @This() {
+    return Self{
+        .allocator = allocator,
+        .source = source,
+        .tokens = .empty,
+    };
+}
+
+fn scanTokens(self: *Self) std.ArrayList(Token) {
+    while (!self.isAtEnd()) {
+        self.start = self.current;
+        self.scanToken();
     }
 
-    fn scanTokens() std.ArrayList(Token) {}
+    self.tokens.append(self.allocator, Token.init(.EOF, "", null, self.line));
+    return self.tokens;
+}
 
-    fn isAtEnd(self: *Scanner) bool {
-        return current >= self.source.len;
+fn sccanToken(self: *Self) void {
+    const c: u8 = self.advance();
+    switch (c) {
+        '(' => self.addToken(.LEFT_PAREN, null),
+        ')' => self.addToken(.RIGHT_PAREN, null),
+        '{' => self.addToken(.LEFT_BRACE, null),
+        '}' => self.addToken(.RIGHT_BRACE, null),
+        ',' => self.addToken(.COMMA, null),
+        '.' => self.addToken(.DOT, null),
+        '-' => self.addToken(.MINUS, null),
+        '+' => self.addToken(.PLUS, null),
+        ';' => self.addToken(.SEMICOLON, null),
+        '*' => self.addToken(.STAR, null),
     }
-};
+}
+
+fn addToken(self: *Self, token_type: Token.TokenType, literal: Token.LiteralValue) void {
+    const text = self.source[self.start..self.current];
+    self.tokens.append(self.allocator, .{ .token_type = token_type, .lexeme = text, .literal = literal });
+}
+
+fn advance(self: *Self) u8 {
+    const char = self.source[self.current];
+    self.current += 1;
+
+    return char;
+}
+fn isAtEnd(self: *Self) bool {
+    return self.current >= self.source.len;
+}
--- src/Token.zig
+++ src/Token.zig
@@ -1,20 +1,73 @@
 const std = @import("std");
-const TokenType = @import("TokenType.zig").TokenType;
-pub const Token = struct {
-    token_type: TokenType,
-    lexeme: []const u8,
-    literal: []const u8,
-    line: u32,
 
-    pub fn init(token_type: TokenType, lexeme: []const u8, literal: []const u8, line: u32) Token {
-        return Token{ .token_type = token_type, .lexeme = lexeme, .literal = literal, .line = line };
-    }
+token_type: TokenType,
+lexeme: []const u8,
+literal: ?LiteralValue,
+line: u32,
+const Self = @This();
 
-    pub fn to_string(self: *Token, allocator: *std.mem.Allocator) []const u8 {
-        return std.fmt.allocPrint(allocator, "{s} {s} {s}", .{
-            self.token_type,
-            self.lexeme,
-            self.literal,
-        });
-    }
+pub fn init(token_type: TokenType, lexeme: []const u8, literal: ?LiteralValue, line: u32) Self {
+    return Self{ .token_type = token_type, .lexeme = lexeme, .literal = literal, .line = line };
+}
+
+pub fn to_string(self: *Self, allocator: *std.mem.Allocator) []const u8 {
+    return std.fmt.allocPrint(allocator, "{s} {s} {s}", .{
+        self.token_type,
+        self.lexeme,
+        self.literal,
+    });
+}
+
+pub const LiteralValue = union(enum) {
+    Nil: void,
+};
+
+pub const TokenType = enum {
+    // Single-character tokens.
+    LEFT_PAREN,
+    RIGHT_PAREN,
+    LEFT_BRACE,
+    RIGHT_BRACE,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SEMICOLON,
+    SLASH,
+    STAR,
+
+    // One or two character tokens.
+    BANG,
+    BANG_EQUAL,
+    EQUAL,
+    EQUAL_EQUAL,
+    GREATER,
+    GREATER_EQUAL,
+    LESS,
+    LESS_EQUAL,
+
+    // Literals.
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords.
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF,
 };
